588bfbbc61c98a5230eb8fa98f51a57937feefc4
ASHmd97 <ahmed.hmd1996@gmail.com>
2024-05-15 06:10:13 +0300

    add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import "./App.css";
-
 function App() {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-2">
+      <div>
+        <h1 className="text-3xl font-bold underline text-red-500">
+          Hello world!
+        </h1>
+      </div>
+      <div>
+        <p className="text-3xl font-bold underline text-red-500">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor iste
+          quam expedita, unde amet debitis! Labore nostrum voluptatem minus
+          dolores! Doloribus adipisci eaque architecto nisi illo commodi magni
+          eveniet dolor!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
